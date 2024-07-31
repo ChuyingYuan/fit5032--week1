d6d79a863a585cfb447def7b67ac55f044a48efe
kristyyuan <cyua0010@student.monash.edu>
2024-07-31 14:35:56 +1000

    first commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <p>Chuying Yuan</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyName'
+  }
+  </script>
+  
+  <style scoped>
+  p {
+    font-size: 30px;
+    color: #fefefe;
+  }
+  </style>
+  
